06affd39d6df6ba6e241cacf70270de7ecf0f5c3
Helio Barros <barros.helio98@gmail.com>
2023-01-21 20:54:31 -0300

    Tabela em angular

--- crud-frontend/src/app/courses/courses/courses.component.ts
+++ crud-frontend/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front-end'},
+    {_id: '2', name: 'Spring', category: 'Back-end'},
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor(){
+    // this.courses = [];
+  }
 }
