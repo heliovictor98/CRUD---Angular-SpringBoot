136f59969417e920abb5f89ec9c4e29f18295b85
Helio Barros <barros.helio98@gmail.com>
2023-01-22 15:27:38 -0300

    shered para modulos e scss do visual

--- crud-frontend/src/app/courses/courses.module.ts
+++ crud-frontend/src/app/courses/courses.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
+
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 
 
 
@@ -16,8 +17,8 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule,
-    MatCardModule
+    AppMaterialModule,
+
   ]
 })
 export class CoursesModule { }
