7ac33a6ff3ead5457d8c52b92a428dae229e78bc
Helio Barros <barros.helio98@gmail.com>
2023-01-21 21:06:45 -0300

    ajuste css da tabela

--- crud-frontend/src/app/courses/courses.module.ts
+++ crud-frontend/src/app/courses/courses.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+
 
 
 
@@ -14,7 +16,8 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class CoursesModule { }
